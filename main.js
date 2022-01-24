cf65783bdfa53f33d2afd119536f2a05b1dd79a3
Among-Rex <80576723+Among-Rex@users.noreply.github.com>
2022-01-24 17:06:35 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
-function kitchen() {
-    
+img = "";
+audio = "";
+
+function preload() {
+    img = loadImage('background.jpeg');
+    audio = loadSound('alarm.mp3');
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Person Detected";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(img, 0, 0, 380, 380);
+
+    if (status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(img, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + objects.length;
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
